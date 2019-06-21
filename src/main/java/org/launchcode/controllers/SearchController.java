04c470ccbd32a19199e269fd1786241fe2255fa0
Brian Hirtzer <bhirtzer@gmail.com>
2019-06-20 23:00:33 -0500

    FInished tech-jobs mvc

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -4,10 +4,13 @@ import org.launchcode.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+
+import static org.springframework.web.bind.annotation.RequestMethod.*;
 
 /**
  * Created by LaunchCode
@@ -19,6 +22,23 @@ public class SearchController {
     @RequestMapping(value = "")
     public String search(Model model) {
         model.addAttribute("columns", ListController.columnChoices);
+        return "search";
+    }
+
+    @RequestMapping(value="results", method= RequestMethod.GET)
+    public String search(Model model, @RequestParam String searchTerm, @RequestParam String searchType){
+        model.addAttribute("columns", ListController.columnChoices);
+
+        ArrayList<HashMap<String, String>> jobs = new ArrayList<>();
+
+        if(searchType.equals("all")){
+            jobs = JobData.findByValue(searchTerm);
+        } else {
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+        }
+
+        model.addAttribute("jobs", jobs);
+
         return "search";
     }
 
